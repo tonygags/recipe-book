c6dc9e63a16be822ba5ad422d6cfd89506803aa6
Tony Gagliardi <tonygags@internode.on.net>
2017-09-21 20:49:30 +0930

    First commit

---
+++ client/recipes/Recipes.js
@@ -0,0 +1,22 @@
+//Meteor.subscribe('recipes');
+
+//console.log(Meteor.settings.public.ga.account);
+
+Template.Recipes.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+        self.subscribe('recipes');
+    });
+});
+
+Template.Recipes.helpers({
+    recipes: () => {
+        return Recipes.find({});
+    }
+});
+
+Template.Recipes.events({
+	'click .new-recipe' : () => {
+		Session.set('newRecipe', true);
+	}
+});
